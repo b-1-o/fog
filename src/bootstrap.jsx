31cda7bcdc86453b7974e37e029f63d21a4a8da5
b1o <erikghabuzyan6@gmail.com>
2026-09-16 22:38:03 -0700

    chore: remove obsolete bootstrap entry

--- src/bootstrap.jsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-// main.jsx is kept as the visual scene module; expose createRoot before it is evaluated.
-globalThis.createRoot = createRoot;
-import('./main.jsx');
